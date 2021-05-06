d4c5863f1b096767f601a089cdad7b18ce3cc9d6
gabriel <gabrielratz84@gmai.com>
2021-05-06 07:27:03 +0200

    changes

---
+++ src/Credentials.js
@@ -0,0 +1,8 @@
+const Credentials = () => {
+  return {
+    ClientId: "7e1f8230e016492f9f323a4b7f71a14f",
+    ClientSecret: "2968f3f1fb4e4b54b74265d38eda4440",
+  };
+};
+
+export { Credentials };
